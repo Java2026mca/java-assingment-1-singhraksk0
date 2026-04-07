e050ec0e88bbd96b3b8d198bdba93efbb94337f7
RAKSHA SINGH <singhraksk0@gmail.com>
2026-04-07 11:31:52 +0530

    solveq1

--- src/q02/Main.java
+++ src/q02/Main.java
@@ -1,21 +1,45 @@
-import java.util.Scanner;
+import java.util.*;
 
-public class PascalsTriangle {
+public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
 
-        for (int i = 0; i < n; i++) {
-            int num = 1;
-            for (int j = 0; j <= i; j++) {
-                System.out.print(num);
-                
-                if (j < i) {
+        // Upper part
+        for (int i = 1; i <= n; i++) {
+
+            // spaces before
+            for (int j = 1; j <= n - i; j++) {
+                System.out.print(" ");
+            }
+
+            // hollow stars
+            for (int j = 1; j <= 2 * i - 1; j++) {
+                if (j == 1 || j == 2 * i - 1) {
+                    System.out.print("*");
+                } else {
                     System.out.print(" ");
                 }
-
-                num = num * (i - j) / (j + 1);
             }
+
+            System.out.println();
+        }
+
+        // Lower part
+        for (int i = n - 1; i >= 1; i--) {
+
+            for (int j = 1; j <= n - i; j++) {
+                System.out.print(" ");
+            }
+
+            for (int j = 1; j <= 2 * i - 1; j++) {
+                if (j == 1 || j == 2 * i - 1) {
+                    System.out.print("*");
+                } else {
+                    System.out.print(" ");
+                }
+            }
+
             System.out.println();
         }
     }
