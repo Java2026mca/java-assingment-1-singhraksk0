9b0696eee8f017589a7a7d57739d5687a1685849
RAKSHA SINGH <singhraksk0@gmail.com>
2026-04-08 00:04:20 +0530

    solveed question 2

--- src/q02/Main.java
+++ src/q02/Main.java
@@ -5,39 +5,17 @@ public class Main {
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
 
-        // Upper part
-        for (int i = 1; i <= n; i++) {
+        for (int i = 0; i < n; i++) {
+            int num = 1;
 
-            // spaces before
-            for (int j = 1; j <= n - i; j++) {
-                System.out.print(" ");
-            }
+            for (int j = 0; j <= i; j++) {
+                System.out.print(num);
 
-            // hollow stars
-            for (int j = 1; j <= 2 * i - 1; j++) {
-                if (j == 1 || j == 2 * i - 1) {
-                    System.out.print("*");
-                } else {
+                if (j < i) {
                     System.out.print(" ");
                 }
-            }
 
-            System.out.println();
-        }
-
-        // Lower part
-        for (int i = n - 1; i >= 1; i--) {
-
-            for (int j = 1; j <= n - i; j++) {
-                System.out.print(" ");
-            }
-
-            for (int j = 1; j <= 2 * i - 1; j++) {
-                if (j == 1 || j == 2 * i - 1) {
-                    System.out.print("*");
-                } else {
-                    System.out.print(" ");
-                }
+                num = num * (i - j) / (j + 1);
             }
 
             System.out.println();
