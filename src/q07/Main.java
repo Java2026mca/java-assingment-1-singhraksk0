e6a982eca9e1d27db2850594bd58587b0568607f
RAKSHA SINGH <singhraksk0@gmail.com>
2026-04-09 10:45:59 +0530

    Update Main.java

--- src/q07/Main.java
+++ src/q07/Main.java
@@ -24,7 +24,7 @@ public class Main {
                 }
             }
         }
-        
+      
         // Print sorted array ascending, space-separated
         for (int i = 0; i < n; i++) {
             System.out.print(arr[i]);
